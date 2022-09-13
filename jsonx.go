4aa2b14bc3f4f60f037c8afab1bc7f6944028f11
Chance <chanceusc@gmail.com>
2022-09-13 12:29:58 -0400

    adds EncodeString

--- jsonx.go
+++ jsonx.go
@@ -3,6 +3,7 @@ package jsonx
 import (
 	"bytes"
 	"unicode"
+	"unicode/utf8"
 )
 
 var (
@@ -14,9 +15,15 @@ var (
 	nullVal  = []byte("null")
 )
 
+type Writer interface {
+	Write(p []byte) (n int, err error)
+	WriteByte(c byte) error
+	WriteString(s string) (n int, err error)
+}
+
 // IsValid reports whether s is a valid JSON number literal.
 //
-// Taken from encoding/json/scanner.go
+// Taken from encoding/json
 func IsNumber(data []byte) bool {
 	// This function implements the JSON numbers grammar.
 	// See https://tools.ietf.org/html/rfc7159#section-6
@@ -74,6 +81,84 @@ func IsNumber(data []byte) bool {
 
 	// Make sure we are at the end.
 	return len(data) == 0
+}
+
+var hex = "0123456789abcdef"
+
+// EncodeString encodes a string to a json string literal, escaping any
+// characters that are not permitted.
+//
+// Logic taken from encoding/json
+func EncodeString(w Writer, s []byte) {
+	escapeHTML := true
+	w.WriteByte('"')
+	start := 0
+	for i := 0; i < len(s); {
+		if b := s[i]; b < utf8.RuneSelf {
+			if htmlSafeSet[b] || (!escapeHTML && safeSet[b]) {
+				i++
+				continue
+			}
+			if start < i {
+				w.Write(s[start:i])
+			}
+			w.WriteByte('\\')
+			switch b {
+			case '\\', '"':
+				w.WriteByte(b)
+			case '\n':
+				w.WriteByte('n')
+			case '\r':
+				w.WriteByte('r')
+			case '\t':
+				w.WriteByte('t')
+			default:
+				// This encodes bytes < 0x20 except for \t, \n and \r.
+				// If escapeHTML is set, it also escapes <, >, and &
+				// because they can lead to security holes when
+				// user-controlled strings are rendered into JSON
+				// and served to some browsers.
+				w.WriteString(`u00`)
+				w.WriteByte(hex[b>>4])
+				w.WriteByte(hex[b&0xF])
+			}
+			i++
+			start = i
+			continue
+		}
+		c, size := utf8.DecodeRune(s[i:])
+		if c == utf8.RuneError && size == 1 {
+			if start < i {
+				w.Write(s[start:i])
+			}
+			w.WriteString(`\ufffd`)
+			i += size
+			start = i
+			continue
+		}
+		// U+2028 is LINE SEPARATOR.
+		// U+2029 is PARAGRAPH SEPARATOR.
+		// They are both technically valid characters in JSON strings,
+		// but don't work in JSONP, which has to be evaluated as JavaScript,
+		// and can lead to security holes there. It is valid JSON to
+		// escape them, so we do so unconditionally.
+		// See http://timelessrepo.com/json-isnt-a-javascript-subset for discussion.
+		if c == '\u2028' || c == '\u2029' {
+			if start < i {
+				w.Write(s[start:i])
+			}
+			w.WriteString(`\u202`)
+			w.WriteByte(hex[c&0xF])
+			i += size
+			start = i
+			continue
+		}
+		i += size
+	}
+	if start < len(s) {
+		w.Write(s[start:])
+	}
+	w.WriteByte('"')
 }
 
 func IsNull(d []byte) bool {
---
+++ jsonx_test.go
@@ -0,0 +1,29 @@
+package jsonx_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"testing"
+
+	"github.com/chanced/jsonx"
+)
+
+func TestEncodeString(t *testing.T) {
+	str := `
+<html>
+	<body>
+  		"hello world!"
+	</body>
+</html>`
+
+	expected, err := json.Marshal(str)
+	if err != nil {
+		t.Fatal(err)
+	}
+	b := bytes.Buffer{}
+
+	jsonx.EncodeString(&b, []byte(str))
+	if b.String() != string(expected) {
+		t.Errorf("expected %s, got %s", expected, b.String())
+	}
+}
